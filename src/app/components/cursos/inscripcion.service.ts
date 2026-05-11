bdaea85f451ee2983dffa250812f295141f7a3ed
PATRIUWU\patri <patrick110804238@gmail.com>
2026-05-11 20:24:44 -0300

    Subo la parte 2, servicios de angular

--- src/app/components/cursos/inscripcion.service.ts
+++ src/app/components/cursos/inscripcion.service.ts
@@ -6,15 +6,25 @@ import { Injectable } from '@angular/core';
 export class InscripcionService {
   private listaInscripciones: any[] = [];
 
-  agregarInscripcion(item: any) {
-    this.listaInscripciones.push(item);
-  }
+  constructor() { }
 
+  // Retorna la lista completa [cite: 65, 66]
   obtenerInscripciones() {
     return this.listaInscripciones;
   }
 
+  // Agrega una nueva inscripción al array [cite: 64, 65]
+  agregarInscripcion(item: any) {
+    this.listaInscripciones.push(item);
+  }
+
+  // Calcula el total acumulado de todas las inscripciones [cite: 67]
   calcularRecaudacionTotal() {
     return this.listaInscripciones.reduce((acc, obj) => acc + obj.precioFinal, 0);
+  }
+
+  // Filtra la cantidad de alumnos por categoría para el resumen [cite: 67]
+  obtenerResumenPorCategoria(categoria: string) {
+    return this.listaInscripciones.filter(i => i.categoriaAlumno === categoria).length;
   }
 }
